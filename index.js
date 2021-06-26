ccc93bd4c2ddf7c68594317ef31a9d071d3e7988
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-26 20:27:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 
 ReactDOM.render( 
    <App />,
     document.getElementById('root')  
   );
-
-//   In Pure Javascript
-
-// var h1 = document.createElement('h1');
-// h1.innerHTML = "Hello World";
-// document.getElementById('root').appendChild(h1);
