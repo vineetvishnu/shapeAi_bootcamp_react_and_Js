408d185c9d364bdfc74db4089b4b3f9ee466f7d3
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-26 20:16:49 +0530

    Add files via upload

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './card';
+
+function Body() {
+    return (
+        <body>
+            <div className="main">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+        </body>
+    );
+}
+
+export default Body;
