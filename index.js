4af2f105e36d6bc9c4baacd2de273ee7e590b38e
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-26 20:09:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render( 
+   <App />,
+    document.getElementById('root')  
+  );
+
+//   In Pure Javascript
+
+// var h1 = document.createElement('h1');
+// h1.innerHTML = "Hello World";
+// document.getElementById('root').appendChild(h1);
