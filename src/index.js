28376cee73554db1074718f0eec5cf74fba58f15
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-26 20:17:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,3 @@ ReactDOM.render(
    <App />,
     document.getElementById('root')  
   );
-
-//   In Pure Javascript
-
-// var h1 = document.createElement('h1');
-// h1.innerHTML = "Hello World";
-// document.getElementById('root').appendChild(h1);
