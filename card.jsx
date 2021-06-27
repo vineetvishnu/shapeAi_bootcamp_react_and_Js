af7555ba75764a4620214b78e678bf2e9e06bdcc
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-27 11:28:07 +0530

    Update card.jsx

--- card.jsx
+++ card.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { BiBadgeCheck } from "react-icons/bi";
-import { BiCalendar } from "react-icons/bi";
+
+import { BiBadgeCheck, BiRupee, BiCalendar }  from "react-icons/bi";
 import { AiOutlineProject } from "react-icons/ai";
 import { MdPeopleOutline } from "react-icons/md";
-import reactandjs from './reactandjs.jpg';
 
 
-function Card() {
+
+function Card(props) {
     return (
+        <>
         <div className="card">
-        <img src={reactandjs} /><a href="javascript:void(0)">Free</a>
+        <img src={props.imgsrc} alt=" " /> 
+        <a href="javascript:void(0)" >Free</a> 
         <div className="card-content">
             <div className="card-title">
-              <h2>Bootcamp on ReactJs and <br />JavaScript</h2>
+              <h2>{props.ctitle}</h2>
                 </div>
          <p><BiCalendar className="icons" /><b> 7 day Long</b></p>
          <p><BiBadgeCheck className="icons" /><b> certificates</b></p>
-         <p><AiOutlineProject className="icons" /><b> Hands on Projects</b></p>
+         <p><AiOutlineProject className="icons" /><b> Hands on Project</b></p>
          <p><MdPeopleOutline className="icons" /><b> No prerequisites</b></p>
+         <p><BiRupee className="icons" /><b> Absolutely Free</b></p>
          <hr />
-         <button className="btn">Register</button>
+         <button className="btn" href="javascript:void(0)">Register</button>
         </div>
     </div>
+    </>
     );
 }
 
