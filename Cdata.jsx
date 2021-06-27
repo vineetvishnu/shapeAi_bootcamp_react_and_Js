cdd5ff6090570a4964d8b0065dc87e091d3efc47
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-06-27 11:28:49 +0530

    Add files via upload

---
+++ Cdata.jsx
@@ -0,0 +1,28 @@
+const Cdata =[
+    {
+        imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3n1bNJ7qaKxWAhxPNUKQR-ivs3S2D2TuIUQ&usqp=CAU",
+        ctitle: "Bootcamp on ReactJs and JavaScript"
+    },
+    {
+        imgsrc: "https://www.filepicker.io/api/file/BFMMlbcQvml9HSqXcvNp",
+        ctitle: "Bootcamp on Python and Data Analytics" 
+    },
+    {
+        imgsrc: "https://lh3.googleusercontent.com/proxy/k0RRJ007vb5pXh-nOKIAbm0M1v5xuuKys4vMeQ0VLfh43T-PDKN_Kb1QKIZaso9yXOUxw6mS8iFaQYJksjONYMPY5lStXA1ndZdM3PyMGrSVySG80CAlQg",
+        ctitle: "Bootcamp on Backend Web-Devlopment" 
+    },
+    {
+        imgsrc: "https://s3.us-east-1.amazonaws.com/storage.trumplearning.com/cmsfiles/best-full-stack-web-developer-courses-certification-online-ppjh0d5a.jpg",
+        ctitle: "Bootcamp on Full-Stack Devlopment" 
+    },
+    {
+        imgsrc: "https://s7280.pcdn.co/wp-content/uploads/2016/06/database-blue.png",
+        ctitle: "Bootcamp on Database Management Systems" 
+    },
+    {
+        imgsrc: "https://cdn-res.keymedia.com/cms/images/us/036/0248_637317913147707069.jpg",
+        ctitle: "Bootcamp on Cyber Security" 
+    }
+]
+
+export default Cdata;
